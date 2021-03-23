608a7b33e63d4470577156d15239259fe8c27619
Cleyton Ezequiel de Oliveira <caverninha007@hotmail.com>
2021-03-23 00:53:51 -0300

    Pagina de construcao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ConstructorComponent } from './home/constructor/constructor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConstructorComponent
   ],
   imports: [
     BrowserModule
